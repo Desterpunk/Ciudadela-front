a5afaf3977501cbd76bd93ebbf8b11958d654c5d
Jhon Serna Durango <jhon.serna@sofka.com.co>
2022-01-14 10:30:45 -0500

    Routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MaterialsPageComponent } from './pages/materials-page/materials-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'materials-page',
+    component: MaterialsPageComponent,
+  },
+  {
+    path: 'home-page',
+    component: HomePageComponent,
+  },
+  { path: '',   redirectTo: '/home-page', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MaterialsPageComponent } from './pages/materials-page/materials-page.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FooterComponent } from './components/footer/footer.component';
     HomePageComponent,
     HeaderComponent,
     FooterComponent,
+    MaterialsPageComponent,
   ],
   imports: [
     BrowserModule,
